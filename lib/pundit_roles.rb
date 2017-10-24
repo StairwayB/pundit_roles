b89953aa7cf6f948ae0952e3a06892fb597e233b
Daniel <daniel.balogh@clever-software-solutions.com>
2017-10-24 15:08:34 +0300

    Moved dependies around

--- lib/pundit_roles.rb
+++ lib/pundit_roles.rb
@@ -1,3 +1,8 @@
+require 'active_support/core_ext/hash/slice'
+require 'active_support/core_ext/array/extract_options'
+require 'active_support/core_ext/string/inflections'
+require 'active_support/core_ext/object/blank'
+
 require 'pundit_roles/version'
 require 'pundit_roles/pundit'
 require 'pundit_roles/policy/base'
--- lib/pundit_roles/policy/base.rb
+++ lib/pundit_roles/policy/base.rb
@@ -1,8 +1,3 @@
-require 'active_support/core_ext/hash/slice'
-require 'active_support/core_ext/array/extract_options'
-require 'active_support/core_ext/string/inflections'
-require 'active_support/core_ext/object/blank'
-
 require_relative 'role'
 require_relative 'policy_defaults/defaults'
 
